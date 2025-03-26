d87fa2b894e9d281f304d76154f85c66dde819cf
adamiar <151006031+adamiar@users.noreply.github.com>
2025-03-26 12:28:46 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "allowed_ips" {
   description = "allowed IP adresses for VPC security group"
   # sensitive = true
 }
+
+variable "role_arn" {
+  description = "The ARN of the role to assume"
+  sensitive   = true
+}
